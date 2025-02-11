5ff8d1dcc8d5019ee7e1225731427166d0b5c2eb
nileshadixoo <nilesh.chouhan@adixoo.com>
2025-02-11 14:40:51 +0530

    added bruteforce method

--- addingElement.js
+++ addingElement.js
@@ -2,12 +2,29 @@ function add(arr,pos, value){
     if(pos<0 || pos>arr.length)
         return "Invalid position"
 
-    array.length++;
-    for(let i=arr.length-1; i>pos; i--){
-        arr[i] = arr[i-1];
+
+    // with extra space
+    let newArr = [];
+    for(let i=0; i<pos; i++){
+        newArr[i] = arr[i];
     }
-    arr[pos] = value;
+    arr[pos]=value;
+    for(let i=pos+1; i<arr.length; i++){
+        newArr[i] = arr[i]
+    }
+
+    // without using extra spaces
+    // array.length++;
+    // for(let i=arr.length-1; i>pos; i--){
+    //     arr[i] = arr[i-1];
+    // }
+    // arr[pos] = value;
+
+
+    
     return array;
+
+
 }   
 
 
