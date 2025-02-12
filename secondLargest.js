8a37db10769d94b021ec7044034a3b27855a3d92
nileshadixoo <nilesh.chouhan@adixoo.com>
2025-02-12 10:24:06 +0530

    added optimal approch

--- secondLargest.js
+++ secondLargest.js
@@ -19,5 +19,27 @@ function secondLargest(arr){
     }   
     return sLargest;
 }
+
+
+// optimal approch
+function secondLargest2(arr){
+    if(arr.length <=1) return "Invalid Input"
+    let largest = arr[0];
+    let sLargest = -1;
+    for(let i=1; i<arr.length; i++){
+        if(arr[i]>largest){
+            sLargest = largest;
+            largest = arr[i]
+        }
+       
+        else if(arr[i] < largest && arr[i] > sLargest){
+            sLargest = arr[i];
+        }
+    }
+    return (sLargest === -1) ? "No second Largest Element" :sLargest;
+}
+
 let array = [3,6,9,4,0,3];
 console.log(secondLargest(array))
+console.log(secondLargest2(array));
+
